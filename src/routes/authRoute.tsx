96049e5e15a5bb406a7cf926fac8ced69785364f
vishakhaa05 <vishakhaagrawal0508@gmail.com>
2025-11-30 21:02:54 +0530

    Initial commit after cleaning nested repo

--- src/routes/authRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-const AuthRoute = () => {
- return <Outlet />;
-};
-
-export default AuthRoute;
--- src/routes/protectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-const ProtectedRoute = () => {
-  return <Outlet />;
-};
-
-export default ProtectedRoute;
